1ae3ab750699819ddff832b42d21781284877abc
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-11-28 01:01:35 +0000

    changes made

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -35,7 +35,6 @@ class Settings(BaseSettings):
 
     # CORS - Adjust for your frontend URL
     CORS_ORIGINS = ["https://echo-orcin-seven.vercel.app", "https://yourdomain.com"]
-
     CORS_ALLOW_CREDENTIALS: bool = True
     CORS_ALLOW_METHODS: list[str] = ["*"]
     CORS_ALLOW_HEADERS: list[str] = ["*"]
